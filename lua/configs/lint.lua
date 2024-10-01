cd1abfdd7f554a89ae2f590425bcc5a7b9a798c8
M. Syahrizal Fauzi <m.syahrizal@corp.bri.co.id>
2024-10-01 09:41:01 +0700

    add javascriptreact & typescriptreact to nvim lint

--- lua/configs/lint.lua
+++ lua/configs/lint.lua
@@ -1,6 +1,8 @@
 require("lint").linters_by_ft = {
   javascript = { "eslint" },
   typescript = { "eslint" },
+  javascriptreact = { "eslint" },
+  typescriptreact = { "eslint" },
   json = { "eslint" },
 }
 
