96bb33fe5bdcee8bf7e1edfcfcc7c06d35d5a89a
M. Syahrizal Fauzi <m.syahrizal@corp.bri.co.id>
2024-10-05 10:03:22 +0700

    add dressing & ToggleFormatOnSave

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -12,11 +12,35 @@ local options = {
     php = { "pretty-php" },
   },
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+  -- format_on_save = {
+  --   -- These options will be passed to conform.format()
+  --   timeout_ms = 500,
+  --   lsp_fallback = true,
+  -- },
 }
+
+-- Variable to track whether formatting is enabled
+local format_on_save_enabled = true
+
+-- Function to toggle formatting on save
+local function toggle_format_on_save()
+  format_on_save_enabled = not format_on_save_enabled
+  if format_on_save_enabled then
+    print "Formatting on save is enabled."
+  else
+    print "Formatting on save is disabled."
+  end
+end
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    if format_on_save_enabled then
+      require("conform").format { bufnr = args.buf }
+    end
+  end,
+})
+
+vim.api.nvim_create_user_command("ToggleFormatOnSave", toggle_format_on_save, {})
 
 return options
