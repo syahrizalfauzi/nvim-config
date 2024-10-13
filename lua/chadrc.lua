1ead972f58d059ca0fb7b6b2cb942a9217c954c3
M. Syahrizal Fauzi <m.syahrizal@corp.bri.co.id>
2024-10-13 18:10:15 +0700

    enhance statusline to show directory

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -15,16 +15,32 @@ autocmd("VimEnter", {
 ---@type ChadrcConfig
 local M = {}
 
+-- for statusline to show folder
+vim.cmd "highlight St_relativepath guifg=#626a83 guibg=#2a2b36"
+
+local stbufnr = function()
+  return vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
+end
+
 M.ui = {
   tabufline = {
     order = { "btns", "tabs", "buffers", "treeOffset" },
   },
-  -- tabufline = {
-  --   overriden_modules = function(modules)
-  --     table.insert(modules, modules[1])
-  --     table.remove(modules, 1)
-  --   end,
-  -- },
+  statusline = {
+    theme = "default",
+    order = { "mode", "relativepath", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
+    modules = {
+      relativepath = function()
+        local path = vim.api.nvim_buf_get_name(stbufnr())
+
+        if path == "" then
+          return ""
+        end
+
+        return "%#St_relativepath#  " .. vim.fn.expand "%:.:h" .. " /"
+      end,
+    },
+  },
 }
 M.base46 = {
   theme = "catppuccin",
