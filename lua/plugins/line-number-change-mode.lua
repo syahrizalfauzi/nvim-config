b1670e9269f84e4ebb5c952437680c3f4d4c932e
M. Syahrizal Fauzi <m.syahrizal@corp.bri.co.id>
2024-10-13 08:37:36 +0700

    add line-number-change-mode & ts-comments

---
+++ lua/plugins/line-number-change-mode.lua
@@ -0,0 +1,45 @@
+return {
+  "sethen/line-number-change-mode.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    { "catppuccin/nvim", name = "catppuccin" },
+  },
+  config = function()
+    require("catppuccin").setup {
+      flavour = "mocha",
+    }
+    local palette = require("catppuccin.palettes").get_palette "mocha"
+
+    if palette == nil then
+      return nil
+    end
+
+    require("line-number-change-mode").setup {
+      i = {
+        bg = palette.green,
+        fg = palette.mantle,
+        bold = true,
+      },
+      n = {
+        bg = palette.blue,
+        fg = palette.mantle,
+        bold = true,
+      },
+      R = {
+        bg = palette.maroon,
+        fg = palette.mantle,
+        bold = true,
+      },
+      v = {
+        bg = palette.mauve,
+        fg = palette.mantle,
+        bold = true,
+      },
+      V = {
+        bg = palette.mauve,
+        fg = palette.mantle,
+        bold = true,
+      },
+    }
+  end,
+}
